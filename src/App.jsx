325e4355d650e4e231e182b2531786adad58c8e3
medsearch0 <cngzhnmt3@gmail.com>
2023-12-05 01:58:15 +0300

    upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import WeatherApp from "./components/weatherApp/WeatherApp";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <WeatherApp />
+      </div>
+    </>
+  );
+}
+
+export default App;
